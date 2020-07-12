1f400390fcaa4a6e74716b82081523ad7e8d2860
srmchartroom <sean@chartroomcreative.com>
2020-07-12 04:51:28 -0400

    Created questions array to hold inquirer prompts. Created 1st question for project title.

--- index.js
+++ index.js
@@ -3,3 +3,11 @@ const fs = require("fs");
 const inquirer = require("inquirer");
 const util = require("util");
 const writeFileAsync = util.promisify(fs.writeFile);
+
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'Enter your project title.',
+    },
+],
