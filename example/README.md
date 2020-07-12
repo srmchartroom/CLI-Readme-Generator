677edef9418e27908231a5861520055ddac4bbf3
srmchartroom <sean@chartroomcreative.com>
2020-07-12 05:26:25 -0400

    create example directory. Created async await function to write markdown file, and create description prompt.

---
+++ example/README.md
@@ -0,0 +1,7 @@
+
+    #wdf
+
+    ## Description
+
+    sdf
+  
--- index.js
+++ index.js
@@ -4,10 +4,42 @@ const inquirer = require("inquirer");
 const util = require("util");
 const writeFileAsync = util.promisify(fs.writeFile);
 
-const questions = [
+function promptUser() {
+  return inquirer.prompt([
     {
-        type: 'input',
-        name: 'title',
-        message: 'Enter your project title.',
+      type: "input",
+      name: "title",
+      message: "Enter your project title.",
     },
-],
+    {
+      type: "input",
+      name: "description",
+      message: "Enter your project description.",
+    },
+  ]);
+}
+
+function makeReadMe(answers) {
+  return `
+    #${answers.title}
+
+    ## Description
+
+    ${answers.description}
+  `;
+}
+
+async function init() {
+  try {
+    const answers = await promptUser();
+    const markdown = makeReadMe(answers);
+
+    await writeFileAsync("example/README.md", markdown);
+
+    console.log("Successfully wrote to index.html");
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+init();
